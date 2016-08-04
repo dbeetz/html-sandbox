fbbbce3ddd515cd4e3071122e85ac48e34bb1409
Devon Beets <dbeetzz@gmail.com>
2016-08-04 09:53:27 -0600

    updated version of jQuery

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 		<![endif]-->
 
 		<!-- jQuery -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
